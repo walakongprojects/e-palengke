861ef32ca9ad7e26d8466f0c39a338c1e831b5cd
walakongprojects <zxcvbnmarcord@gmail.com>
2022-02-28 20:37:29 -0500

    bug fixes

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  databaseDev: 'mongodb://localhost/e-palengke',
+  databaseDev: 'mongodb://localhost/e-palengke-dev',
   databaseProd: 'mongodb://isabuhay:l0n3wolf@ds133086.mlab.com:33086/gulapagadgets-dev'
 }
